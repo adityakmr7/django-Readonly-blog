d82a221cacf040296bf5b4d30b83e81936caa9eb
aditya <adityakmr9672@gmail.com>
2019-03-13 17:26:59 +0530

    post model setup added

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Category, Post
+# Register your models here.
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Post)
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,46 @@
+from tinymce import HTMLField
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from slugger import AutoSlugField
+from django.urls import reverse
 # Create your models here.
+User = get_user_model()
+
+
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.slug, filename)
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.username
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    slug = AutoSlugField(populate_from='title')
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    slug = AutoSlugField(populate_from='title')
+    overview = models.CharField(max_length= 200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = HTMLField()
+    comment_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = models.ImageField(
+        upload_to=upload_location,
+        null=True, 
+        blank=True)
+    category = models.ManyToManyField(Category)
+    featured = models.BooleanField()
+    previous_post = models.ForeignKey('self', related_name= 'previous', on_delete=models.SET_NULL, blank=True, null=True)
+    next_post = models.ForeignKey('self', related_name= 'next', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
