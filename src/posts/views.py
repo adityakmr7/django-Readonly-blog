72407efccb2d0ccf69b01fbc6616934efc8157e7
aditya <adityakmr9672@gmail.com>
2019-03-14 15:15:52 +0530

    index page posts rendering done

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from .models import Post, Author, Category
 # Create your views here.
 def index(request):
-    featured = Post.objects.filter(featured = True)
+    featured = Post.objects.filter(featured = True)   #put this on carousel
+    latest_post = Post.objects.order_by('-timestamp')[:6]
+    startup_post = Post.objects.filter(category__title__iexact='startup')[0:3]
+    opinion_post = Post.objects.filter(category__title__iexact='opinion')[0:3]
+    # add a video field here
     context = {
-        'object_list': featured
+        'object_list': featured,
+        'latest_post': latest_post,
+        'startup_post': startup_post,
+        'opinion_post': opinion_post
     }
     return render(request, 'index.html', context)
